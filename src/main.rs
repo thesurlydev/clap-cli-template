250a85b8c288e472aaa487c4bb4822f099664455
shane <shane@digitalsanctum.com>
2022-06-04 16:05:17 -0700

    Update example main with async reqwest example

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,61 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-use clap::{AppSettings, Parser, Subcommand};
+use clap::{Parser, Subcommand};
+use reqwest::Response;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
     let cli: Cli = Cli::parse();
     match &cli.command {
         Command::Add { name, r#type } => {
             println!("type: {}, name: {}", r#type, name);
         }
-        Command::Info { name } => todo!(),
-        Command::List { name } => todo!(),
-    }
+        Command::Info { .. } => todo!(),
+        Command::List { .. } => todo!(),
+        Command::Get { url } => {
+            let response = do_get(url).await?;
+            let status = response.status();
+            println!("Status: {}", status);
 
-    // TODO do stuff
+            eprintln!("Headers: {:#?}\n", response.headers());
+
+            let body = response.text().await?;
+            println!("body={:?}", body);
+        }
+    }
+    Ok(())
+}
+
+async fn do_get(url: &String) -> reqwest::Result<Response> {
+    reqwest::get(url).await
 }
 
 #[derive(Subcommand)]
 enum Command {
-    #[clap(alias = "a", about = "Add a component", setting(AppSettings::ArgRequiredElseHelp))]
+    #[clap(alias = "a", about = "Add a component")]
     Add {
         #[clap()]
         /// The type of component. Valid values are: alias, func, secret, or tool
         r#type: String,
 
         /// The name of the component
-        name: String
+        name: String,
     },
-    #[clap(alias = "i", about = "Get information about a component", setting(AppSettings::ArgRequiredElseHelp))]
-    Info {
 
-        name: String
+    #[clap(alias = "g", about = "Get a URL")]
+    Get {
+        url: String
     },
-    #[clap(alias = "l", about = "List all components", setting(AppSettings::ArgRequiredElseHelp))]
+
+    #[clap(alias = "i", about = "Get information about a component")]
+    Info { name: String },
+    #[clap(alias = "l", about = "List all components")]
     List { name: String },
 }
 
 #[derive(Parser)]
-#[clap(version, author, about, setting = AppSettings::ArgRequiredElseHelp)]
+#[clap(version, author, about)]
 struct Cli {
     #[clap(subcommand)]
     command: Command,
